90cb8b7db40baa388734e8e6ff58254cb955d38c
Riley Sun <32978625+RileySun@users.noreply.github.com>
2025-06-19 12:31:33 +0200

    Web Update
    Added more control functionality while using popup

--- html/static/index.js
+++ html/static/index.js
@@ -1,28 +1,74 @@
+let state = "main";
+let popupCount = 0;
+
+//Popup
 function selectImage(elem) {
 	document.getElementById("Popup-Text").innerHTML = elem.getAttribute("title");
 	document.getElementById("Popup-Image").src = elem.src;
 	document.getElementById("Popup").style.display = "block";
+	state = "popup"
 }
 
 function closePopup() {
 	document.getElementById("Popup").style.display = "none";
+	state = "main"
 }
 
-document.addEventListener("keyup", (e) => {
-	switch (e.key) {
-		case "Escape":
-			closePopup()
-			return
-		case "ArrowLeft":
-			const prev = document.getElementById("Prev")
-			if (prev == null) {
-				return
-			}
-			window.location.replace(prev.href)
-			return
-		case "ArrowRight":
-			window.location.replace(document.getElementById("Next").href) 
-			return
+//Controls
+function controls(e) {
+	switch (state) {
+		case "main":
+			mainControls(e);
+		case "popup":
+			popupControls(e);
 		default:
 	}
-}, false)
+}
+
+function mainControls(e) {
+	switch (e.key) {
+		case "ArrowLeft":
+			const prev = document.getElementById("Prev");
+			if (prev == null) {
+				return;
+			}
+			window.location.replace(prev.href);
+			return;
+		case "ArrowRight":
+			window.location.replace(document.getElementById("Next").href);
+			return;
+		default:
+	}
+}
+
+function popupControls(e) {
+	const cameras = document.getElementsByClassName("Camera-Src")
+	const cams = Array.from(cameras)
+
+	switch (e.key) {
+		case "Escape":
+			closePopup();
+			return
+		case "ArrowLeft":
+			if (popupCount > 0) {
+				popupCount -= 1
+			} else {
+				popupCount = cameras.length
+			}
+			document.getElementById("Popup-Text").innerHTML = cameras[popupCount].getAttribute("title");
+			document.getElementById("Popup-Image").src = cameras[popupCount].src;
+			return;
+		case "ArrowRight":
+			if (popupCount < cameras.length) {
+				popupCount += 1
+			} else {
+				popupCount = 0
+			}
+			document.getElementById("Popup-Text").innerHTML = cameras[popupCount].getAttribute("title");
+			document.getElementById("Popup-Image").src = cameras[popupCount].src;
+			return;
+		default:
+	}
+}
+
+document.addEventListener("keyup", (e) => {controls(e)}, false)
